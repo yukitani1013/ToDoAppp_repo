a9d4aa795ef28ce499964053a8d2da6bd9b2f01f
BLINK1013 <92933766+blink1013@users.noreply.github.com>
2023-03-28 14:25:39 +0900

    todo list page

---
+++ lib/view/todo_list_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:todo/view/todo_add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  TodoListPageState createState() => TodoListPageState();
+}
+
+class TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ISDL Todo List',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () async {
+          final newTask = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return const TodoAddPage();
+            }),
+          );
+          if (newTask != null) {
+            setState(() {
+              todoList.add(newTask);
+            });
+          }
+        },
+      )
+    );
+  }
+}
